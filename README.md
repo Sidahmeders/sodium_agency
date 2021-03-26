330af85ea37ee357f512cc739a6df38c30900d29
sidahmed <ahorob57@gmail.com>
2021-03-26 10:47:15 +0100

    commit msg

---
+++ README.md
@@ -0,0 +1,18 @@
+# Digital - landing page for web design agency
+
+### Features
+- Has 8 different sections
+- Fully responsive
+
+### Technologies
+- HTML5
+- CSS3 (SCSS)
+- JS (ES6)
+
+### Installation
+`$ npm i`
+
+### Resources
+- [Google fonts](https://fonts.google.com/ "Google fonts") - free fonts
+- [Pexels](https://www.pexels.com "Pexels") - free stock photos
+- [Icomoon](https://icomoon.io/ "Icomoon") - free icons
---
+++ src/app.js
@@ -0,0 +1,15 @@
+import './js/sticky-nav';
+import './js/smooth-scroll';
+import './js/mobile-nav';
+import './js/testimonials-slider';
+
+import './scss/main.scss';
+import './scss/header.scss';
+import './scss/about-us.scss';
+import './scss/our-team.scss';
+import './scss/our-work.scss';
+import './scss/our-advantages.scss';
+import './scss/services.scss';
+import './scss/testimonials.scss';
+import './scss/get-in-touch.scss';
+import './scss/footer.scss';
